9a6164550207128dd2dbf072498bd68b598e229c
progfay <progfay@gmail.com>
2022-03-15 00:03:14 +0900

    test(rgb): add testcase

--- app/rgb_test.rb
+++ app/rgb_test.rb
@@ -5,5 +5,6 @@ class RgbTest < Minitest::Test
   def test_to_hex
     assert_equal '#000000', to_hex(0, 0, 0)
     assert_equal '#FFFFFF', to_hex(255, 255, 255)
+    assert_equal '#043C78', to_hex(4, 60, 120)
   end
 end
