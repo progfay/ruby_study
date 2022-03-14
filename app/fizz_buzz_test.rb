6db80284df15b8c87b22ee0d5d22bced209d3e2b
progfay <progfay@gmail.com>
2022-03-14 23:27:25 +0900

    test(fizz_buzz): add test with minitest gem

---
+++ app/fizz_buzz_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require './fizz_buzz.rb'
+
+class FizzBuzzText < Minitest::Test
+  def test_fizz_buzz
+    assert_equal "FizzBuzz", fizz_buzz(15)
+    assert_equal "FizzBuzz", fizz_buzz(30)
+  end
+
+  def test_buzz
+    assert_equal "Fizz", fizz_buzz(3)
+    assert_equal "Fizz", fizz_buzz(6)
+  end
+
+  def test_fizz
+    assert_equal "Buzz", fizz_buzz(5)
+    assert_equal "Buzz", fizz_buzz(10)
+  end
+
+  def test_others
+    assert_equal "1", fizz_buzz(1)
+    assert_equal "2", fizz_buzz(2)
+  end
+end
