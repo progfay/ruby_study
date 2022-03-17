40c3698ec677c84ab6564ba41f467e8bc8582635
progfay <progfay@gmail.com>
2022-03-17 21:30:19 +0900

    test(convert_length): add testcase

--- app/convert_length_test.rb
+++ app/convert_length_test.rb
@@ -4,5 +4,6 @@ require './convert_length.rb'
 class ConvertLengthText < Minitest::Test
   def test_convert_length
     assert_equal 39.37, convert_length(1, 'm', 'in')
+    assert_equal 0.38, convert_length(15, 'in', 'm')
   end
 end
