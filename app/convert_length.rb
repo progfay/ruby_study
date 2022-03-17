4bbda2cab87c769e0f2d7b47a98b32b67d7447ed
progfay <progfay@gmail.com>
2022-03-17 23:49:16 +0900

    refactor(convert_length): transform hash

--- app/convert_length.rb
+++ app/convert_length.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-UNITS = { :meter => 1.0, :feet => 3.28, :inch => 39.37 }
+UNITS = { meter: 1.0, feet: 3.28, inch: 39.37 }
 
 def convert_length(length, from: :meter, to: :meter)
   (length / UNITS[from] * UNITS[to]).round(2)
