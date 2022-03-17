4161ce2b454253676cd3d464667228d1e73c78c7
progfay <progfay@gmail.com>
2022-03-17 22:36:55 +0900

    refactor(convert_length): use symbol

--- app/convert_length.rb
+++ app/convert_length.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-def convert_length(length, unit_from, unit_to)
-  units = { 'm' => 1.0, 'ft' => 3.28, 'in' => 39.37 }
-  (length / units[unit_from] * units[unit_to]).round(2)
+def convert_length(length, from: :meter, to: :meter)
+  units = { :meter => 1.0, :feet => 3.28, :inch => 39.37 }
+  (length / units[from] * units[to]).round(2)
 end
--- app/convert_length_test.rb
+++ app/convert_length_test.rb
@@ -3,8 +3,8 @@ require './convert_length.rb'
 
 class ConvertLengthText < Minitest::Test
   def test_convert_length
-    assert_equal 39.37, convert_length(1, 'm', 'in')
-    assert_equal 0.38, convert_length(15, 'in', 'm')
-    assert_equal 10670.73, convert_length(35000, 'ft', 'm')
+    assert_equal 39.37, convert_length(1, from: :meter, to: :inch)
+    assert_equal 0.38, convert_length(15, from: :inch, to: :meter)
+    assert_equal 10670.73, convert_length(35000, from: :feet, to: :meter)
   end
 end
