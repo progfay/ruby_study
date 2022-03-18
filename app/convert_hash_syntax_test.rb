48296af72caa8bcdadee20e3de4ea6e4ec342483
progfay <progfay@gmail.com>
2022-03-18 16:50:23 +0900

    test(convert_hash_syntax): add testcase

--- app/convert_hash_syntax_test.rb
+++ app/convert_hash_syntax_test.rb
@@ -4,5 +4,21 @@ require './convert_hash_syntax.rb'
 class ConvertHashSyntaxTest < Minitest::Test
   def test_convert_hash_syntax
     assert_equal '{}', convert_hash_syntax('{}')
+
+    old_syntax = <<~TEXT
+      {
+        :name => 'Alice',
+        :age=>20,
+        :gender => :female
+      }
+    TEXT
+    expected = <<~TEXT
+      {
+        name: 'Alice',
+        age: 20,
+        gender: :female
+      }
+    TEXT
+    assert_equal expected, convert_hash_syntax(old_syntax)
   end
 end
