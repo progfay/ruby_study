1c8ffd6bb07661f03bb37c2a4931b6ab7f5dc5d5
progfay <progfay@gmail.com>
2022-03-19 18:18:27 +0900

    test(gate): add testcase

--- app/gate_test.rb
+++ app/gate_test.rb
@@ -2,12 +2,21 @@ require 'minitest/autorun'
 require './gate.rb'
 
 class GateTest < Minitest::Test
-	def test_gate
+	def test_umeda_to_juso_when_fare_is_enough
 		umeda = Gate.new(:umeda)
 		juso = Gate.new(:juso)
 
 		ticket = Ticket.new(150)
 		umeda.enter(ticket)
 		assert juso.exit(ticket)
+	end
+
+	def test_umeda_to_mikuni_when_fare_is_not_enough
+		umeda = Gate.new(:umeda)
+		mikuni = Gate.new(:mikuni)
+
+		ticket = Ticket.new(150)
+		umeda.enter(ticket)
+		refute mikuni.exit(ticket)
 	end
 end
