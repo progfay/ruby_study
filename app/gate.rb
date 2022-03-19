9f841f4341554d181aa6097df53ed352fddc13d7
progfay <progfay@gmail.com>
2022-03-19 18:16:25 +0900

    feat(gate): make mock more detail

--- app/gate.rb
+++ app/gate.rb
@@ -1,2 +1,18 @@
 class Gate
+	def initialize(name)
+		@name = name
+	end
+
+	def enter(ticket)
+	end
+
+	def exit(ticket)
+		true
+	end
+end
+
+class Ticket
+	def initialize(fare)
+		@fare = fare
+	end
 end
