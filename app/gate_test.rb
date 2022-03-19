ad5d2a6accd7ee1753518448a78efffd983fc0d3
progfay <progfay@gmail.com>
2022-03-19 18:16:13 +0900

    test(gate): make more detail

--- app/gate_test.rb
+++ app/gate_test.rb
@@ -2,7 +2,12 @@ require 'minitest/autorun'
 require './gate.rb'
 
 class GateTest < Minitest::Test
-  def test_gate
-		assert Gate.new
-  end
+	def test_gate
+		umeda = Gate.new(:umeda)
+		juso = Gate.new(:juso)
+
+		ticket = Ticket.new(150)
+		umeda.enter(ticket)
+		assert juso.exit(ticket)
+	end
 end
