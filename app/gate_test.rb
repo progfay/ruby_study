bbdf10053087e3c96696ee1cf161fab645479cbf
progfay <progfay@gmail.com>
2022-03-19 22:47:36 +0900

    test(gate): add testcases

--- app/gate_test.rb
+++ app/gate_test.rb
@@ -14,9 +14,21 @@ class GateTest < Minitest::Test
 		assert @juso.exit(ticket)
 	end
 
-	def test_umeda_to_mikuni
+	def test_umeda_to_mikuni_when_fare_is_not_enough
 		ticket = Ticket.new(150)
 		@umeda.enter(ticket)
 		refute @mikuni.exit(ticket)
+	end
+
+	def test_umeda_to_mikuni_when_fare_is_enough
+		ticket = Ticket.new(190)
+		@umeda.enter(ticket)
+		assert @mikuni.exit(ticket)
+	end
+
+	def test_juso_to_mikuni
+		ticket = Ticket.new(150)
+		@juso.enter(ticket)
+		assert @mikuni.exit(ticket)
 	end
 end
