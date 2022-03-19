e43d738f4f9607960d1bec3fc6cefbc3f5b2bf1b
progfay <progfay@gmail.com>
2022-03-19 22:44:38 +0900

    refactor(gate): use setup func

--- app/gate_test.rb
+++ app/gate_test.rb
@@ -2,21 +2,21 @@ require 'minitest/autorun'
 require './gate.rb'
 
 class GateTest < Minitest::Test
-	def test_umeda_to_juso
-		umeda = Gate.new(:umeda)
-		juso = Gate.new(:juso)
+	def setup
+		@umeda = Gate.new(:umeda)
+		@juso = Gate.new(:juso)
+		@mikuni = Gate.new(:mikuni)
+	end
 
+	def test_umeda_to_juso
 		ticket = Ticket.new(150)
-		umeda.enter(ticket)
-		assert juso.exit(ticket)
+		@umeda.enter(ticket)
+		assert @juso.exit(ticket)
 	end
 
 	def test_umeda_to_mikuni
-		umeda = Gate.new(:umeda)
-		mikuni = Gate.new(:mikuni)
-
 		ticket = Ticket.new(150)
-		umeda.enter(ticket)
-		refute mikuni.exit(ticket)
+		@umeda.enter(ticket)
+		refute @mikuni.exit(ticket)
 	end
 end
