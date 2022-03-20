3252ba55c7fc7919440b95c3d79394727b64d60a
progfay <progfay@gmail.com>
2022-03-20 20:34:50 +0900

    fix(deep_freezable): fix hash notation

--- app/bank.rb
+++ app/bank.rb
@@ -3,5 +3,5 @@ require './deep_freezable.rb'
 class Bank
   extend DeepFreezable
 
-  CURRENCIES = deep_freeze(['Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'])
+  CURRENCIES = deep_freeze({ 'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee' })
 end
