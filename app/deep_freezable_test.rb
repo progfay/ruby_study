c324ac0a1483914eead81372a4f7387905a20804
progfay <progfay@gmail.com>
2022-03-20 20:35:03 +0900

    test(deep_freezable): add testcase

--- app/deep_freezable_test.rb
+++ app/deep_freezable_test.rb
@@ -3,9 +3,18 @@ require './team.rb'
 require './bank.rb'
 
 class DeepFreezableTest < Minitest::Test
-	def test_deep_freezable
+	def test_deep_freezable_to_array
 		assert_equal ['Japan', 'US', 'India'], Team::COUNTRIES
 		assert Team::COUNTRIES.frozen?
 		assert Team::COUNTRIES.all? { |country| country.frozen? }
+	end
+
+	def test_deep_freezable_to_hash
+		assert_equal(
+			{ 'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee' },
+			Bank::CURRENCIES
+		)
+		assert Bank::CURRENCIES.frozen?
+		assert Bank::CURRENCIES.all? { |key, value| key.frozen? && value.frozen? }
 	end
 end
