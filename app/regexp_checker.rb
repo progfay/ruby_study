efa24e0655aa51753e9331538b815f2c849d0ed9
progfay <progfay@gmail.com>
2022-03-24 00:42:30 +0900

    feat(regexp_checker): retry when input pattern is invalid

--- app/regexp_checker.rb
+++ app/regexp_checker.rb
@@ -1,11 +1,16 @@
 print 'Text?: '
 text = gets.chomp
-print 'Pattern?: '
-pattern = gets.chomp
 
-regexp = Regexp.new(pattern)
+begin
+  print 'Pattern?: '
+  pattern = gets.chomp
+  regexp = Regexp.new(pattern)
+rescue RegexpError => e
+  puts "Invalid pattern: #{e.message}"
+  retry
+end
+
 matches = text.scan(regexp)
-
 if matches.size > 0
   puts "Matched: #{matches.join(', ')}"
 else
