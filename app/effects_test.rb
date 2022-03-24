a73eec442865ea8c1c9e6e8e08a3749295764297
progfay <progfay@gmail.com>
2022-03-24 15:11:42 +0900

    test(word_sysnth): fix test

--- app/effects_test.rb
+++ app/effects_test.rb
@@ -3,6 +3,7 @@ require './effects.rb'
 
 class EffectsTest < Minitest::Test
   def test_reverse
-    assert Effects
+    effect = Effects.reverse
+    assert_equal 'ybuR si !nuf', effect.call('Ruby is fun!')
   end
 end
